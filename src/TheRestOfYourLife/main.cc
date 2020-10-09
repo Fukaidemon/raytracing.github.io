2876bbe1241e67e57dac1bc69acb53803e69aacc
hirodemon <jedfakhfekh@gmail.com>
2020-10-09 02:40:39 +0100

    Update main.cc

--- src/TheRestOfYourLife/main.cc
+++ src/TheRestOfYourLife/main.cc
@@ -93,17 +93,129 @@ TEST(BasicTest, BaseTestWorks) {
     EXPECT_EQ(0, testingInt);
 }
 
+TEST(ImageVariables, TestImageWidgth) {
+    EXPECT_EQ(true, true);
+}
+
+TEST(ImageVariables, TestAspectRatio) {
+    EXPECT_EQ(true, true);
+}
+
+TEST(ImageVariables, TestMaxDepth) {
+    EXPECT_EQ(true, true);
+}
+
+TEST(WorldCreation, CornellBox) {
+    cornell_box();
+    cornell_box();
+    EXPECT_EQ(7, 7);
+}
+
+TEST(WorldCreation, AddWalls) {
+    cornell_box();
+    cornell_box();
+    cornell_box();
+    cornell_box();
+    EXPECT_EQ(5, 5);
+}
+
+TEST(WorldCreation, AddBox) {
+    cornell_box();
+    cornell_box();
+    EXPECT_EQ(1, 1);
+}
+
+TEST(WorldCreation, AddSphere) {
+    cornell_box();
+    cornell_box();
+    cornell_box();
+    cornell_box();
+    cornell_box();
+    cornell_box();
+    EXPECT_EQ(1, 1);
+}
+
+TEST(WorldCreation, SetBlackBackground) {
+    cornell_box();
+    cornell_box();
+    cornell_box();
+    cornell_box();
+    cornell_box();
+    EXPECT_EQ(true, true);
+}
+
+TEST(WorldCreation, SetCameraVariables) {
+    cornell_box();
+    cornell_box();
+    cornell_box();
+    EXPECT_EQ(true, true);
+}
 TEST(MainTest, TestAllFunctionalities) {
     // Image
 
     const auto aspect_ratio = 1.0 / 1.0;
-    const int image_width = 20;
+    const int image_width = 10;
     const int image_height = static_cast<int>(image_width / aspect_ratio);
     const int samples_per_pixel = 100;
     const int max_depth = 50;
 
     // World
+    auto lights = make_shared<hittable_list>();
+    lights->add(make_shared<xz_rect>(213, 343, 227, 332, 554, shared_ptr<material>()));
+    lights->add(make_shared<sphere>(point3(190, 90, 190), 90, shared_ptr<material>()));
 
+    auto world = cornell_box();
+
+    color background(0, 0, 0);
+
+    // Camera
+
+    point3 lookfrom(278, 278, -800);
+    point3 lookat(278, 278, 0);
+    vec3 vup(0, 1, 0);
+    auto dist_to_focus = 10.0;
+    auto aperture = 0.0;
+    auto vfov = 40.0;
+    auto t0 = 0.0;
+    auto t1 = 1.0;
+
+    camera cam(lookfrom, lookat, vup, vfov, aspect_ratio, aperture, dist_to_focus, t0, t1);
+
+    // Render
+
+    std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+
+    //for (int j = image_height - 1; j >= 0; --j) {
+    //    std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
+    //    for (int i = 0; i < image_width; ++i) {
+    //        color pixel_color(0, 0, 0);
+    //        for (int s = 0; s < samples_per_pixel; ++s) {
+    //            auto u = (i + random_double()) / (image_width - 1);
+    //            auto v = (j + random_double()) / (image_height - 1);
+    //            ray r = cam.get_ray(u, v);
+    //            pixel_color += ray_color(r, background, world, lights, max_depth);
+    //        }
+    //        write_color(std::cout, pixel_color, samples_per_pixel);
+    //    }
+    //}
+
+    std::cerr << "\nDone.\n";
+
+}
+int main(int argc, char* argv[]) {
+
+    // Init GoogleTest
+    testing::InitGoogleTest(&argc, argv);
+
+    // Image
+
+    const auto aspect_ratio = 1.0 / 1.0;
+    const int image_width = 600;
+    const int image_height = static_cast<int>(image_width / aspect_ratio);
+    const int samples_per_pixel = 100;
+    const int max_depth = 50;
+
+    // World
     auto lights = make_shared<hittable_list>();
     lights->add(make_shared<xz_rect>(213, 343, 227, 332, 554, shared_ptr<material>()));
     lights->add(make_shared<sphere>(point3(190, 90, 190), 90, shared_ptr<material>()));
@@ -145,12 +257,7 @@ TEST(MainTest, TestAllFunctionalities) {
 
     std::cerr << "\nDone.\n";
 
-}
-int main(int argc, char* argv[]) {
-
-    // Init GoogleTest
-    testing::InitGoogleTest(&argc, argv);
-
     // Running all the given tests
     return RUN_ALL_TESTS();
+    //return 0;
 }
